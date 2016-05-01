430a66a495a805712b8834463d719955a12ff911
Alejandro Mauricio Coca Rojas <alxmcr@outlook.com>
2016-05-01 18:43:22 -0400

    Model: New method class 'all'

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -9,5 +9,14 @@ class Racer
     self.mongo_client['racers']
   end
 
+  def self.all(prototype={...}, sort={...}, skip=0, limit=nil)
+
+    results = self.collection.find(prototype).sort(sort).skip(skip)
+    results = results.limit(limit) if !limit.nil?
+
+    return results
+    
+  end
+
 
 end
