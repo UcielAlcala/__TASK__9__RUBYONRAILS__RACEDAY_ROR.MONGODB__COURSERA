cc1556e1ac3dee47187eef0c3b6ce2d565c0a8b2
Alejandro M. Coca <alxmcr@outlook.com>
2016-05-01 21:15:06 -0400

    Model: New method 'update'

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -67,4 +67,19 @@ class Racer
     @id = result.inserted_id
   end
 
+  def update(params)
+    @number = params[:number].to_i
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @gender = params[:gender]
+    @group = params[:group]
+    @secs = params[:secs].to_i
+
+    params.slice!(:number, :first_name, :last_name, :gender, :group, :secs)
+
+    self.class.collection
+  	            .find(:_id=>BSON::ObjectId.from_string(@id))
+  	            .update_one(params)
+  end
+
 end
